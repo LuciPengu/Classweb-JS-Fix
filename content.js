62b8944ed23734cbf8baa01f0801baddcd1c6f9f
Luci <49926403+LuciPengu@users.noreply.github.com>
2023-08-22 18:32:22 -0700

    Add files via upload

--- content.js
+++ content.js
@@ -4,12 +4,17 @@ const link = document.createElement("link");
 link.rel = "stylesheet";
 link.href = browser.extension.getURL("styles.css");
 
-const navbar = document.querySelectorAll('table[summary="This table displays Tab Items."] > a');
+const navbar = document.querySelectorAll('.taboff > a, .tabon > a');
+const newNavLinks = ["bwskflib.P_SelDefTerm","bwskfshd.P_CrseSchd","bwskotrn.P_ViewTermTran","bzskvate.P_VateaOccsGoal"];
+const newNavNames = ["Add Classes", "My Schedule", "My Grades", "Pay Fees"];
 console.log(navbar);
+let i = 0
 navbar.forEach((anchor) => {
 	console.log(anchor.href);
   // Modify the href attribute
-  anchor.href = 'pornhub.com';
+  anchor.href = newNavLinks[i];
+	anchor.innerHTML = newNavNames[i];
+	i ++;
 });
 
 
